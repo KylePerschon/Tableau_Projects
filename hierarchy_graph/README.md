8aca843f58621e82be18144109c57ad095937bad
Kyle <kyleperschon@gmail.com>
2025-03-12 10:03:42 -0600

    cleaned code. added some images

---
+++ hierarchy_graph/README.md
@@ -0,0 +1,4 @@
+# Hierarchy Graph
+The Hierarchy Graph example shows how we can take any list of items (within Pythons recursion bounds (3000 nested)) and turn it into a tree-hierarchy styled graph. This can be useful when you are trying to vizualize the relationship between one or many nodes. The code will take the list of relationships (start and end points) and turn that into an X,Y cordinate pair graph that does not allow for overlap. The dataset can be easily imported into Tableau, and after dropping a few simple pills.. You have a beautiful tree-hierarchy chart!
+
+## 
--- hierarchy_graph/build_x_y_from_start_end_points.py
+++ hierarchy_graph/build_x_y_from_start_end_points.py
@@ -1,11 +1,12 @@
 # %%
 import pandas as pd
 import numpy as np
-from os import environ
 
 
-def rename_duplicate_rows(df, column_name):
+def rename_duplicate_rows(df: pd.DataFrame, column_name:str) -> pd.DataFrame:
     """Renames duplicate row values in a specified column by appending a counter.
+    We need this functionality if the user declares that the same node connects
+    to two or more upstream targets.
 
     Args:
         df (pd.DataFrame): The input DataFrame.
@@ -16,7 +17,6 @@ def rename_duplicate_rows(df, column_name):
     """
     value_counts = df[column_name].value_counts()
     duplicates = value_counts[value_counts > 1].index.tolist()
-
     for value in duplicates:
         count = 1
         for index, row in df.iterrows():
@@ -24,6 +24,98 @@ def rename_duplicate_rows(df, column_name):
                 df.loc[index, column_name] = f"{value}_{count}"
                 count += 1
     return df
+
+
+def build_node_x_cords(data_frame: pd.DataFrame, start_point:str, _x:int = 1, _data:list = None):
+    """_summary_
+
+    Args:
+        data_frame (pd.DataFrame): _description_
+        start_point (str): _description_
+        _x (int, optional): _description_. Defaults to 1.
+        _data (list, optional): _description_. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
+    temp_df = data_frame[data_frame['end_point'] == start_point].reset_index()
+    node_cluster_list = "_".join(temp_df['start_point'].to_list())
+    #node_cluster_list = temp_df['start_point'].to_list()
+    levels_count = len(temp_df) if len(temp_df) > 0 else 1
+    for i, row in temp_df.iterrows():
+        next_start_point = row['start_point']
+        node_cords = {'node':next_start_point, 'x_cord':_x, 'target':start_point, 'node_cluster_count': levels_count, 'node_cluster_list': node_cluster_list, 'branch_order': i+1}
+        if _data:
+            _data.append(node_cords)
+        else:
+            _data = [node_cords]
+        build_node_x_cords(data_frame=data_frame, start_point=next_start_point, _x=_x+1, _data=_data)
+    return _data
+
+def get_max_nodes_in_branch(data_frame: pd.DataFrame, node:str) -> int:
+    """For any given node, find out how the max number of nodes in all
+    sub branches.
+
+    Args:
+        data_frame (pd.DataFrame): Dataframe containing the start and end point list.
+        node (str): The node you want to want to calculate.
+
+    Returns:
+        int: Max number of nodes within all downstream sub-branches.
+    """
+    temp_df = data_frame[data_frame['end_point'] == node].reset_index()
+    total_count = len(temp_df) if len(temp_df) > 0 else 1
+    down_stream_cnt = 0
+    for i, row in temp_df.iterrows():
+        next_node = row['start_point']
+        max_down_nodes = get_max_nodes_in_branch(data_frame=data_frame, start_point=next_node)
+        down_stream_cnt += max_down_nodes
+    return total_count if total_count > down_stream_cnt else down_stream_cnt
+    
+
+def assign_y_values(data_frame:pd.DataFrame, start_point:str, min_range:int, max_range: int, y_values_list: list = None):
+    """_summary_
+
+    Args:
+        data_frame (pd.DataFrame): _description_
+        start_point (str): _description_
+        min_range (int): _description_
+        max_range (int): _description_
+        y_values_list (list, optional): _description_. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
+    y_val = assign_y_from_range(min_range=min_range, max_range=max_range)
+    if not y_values_list:
+        y_values_list = []
+    y_values_list.append({'node':start_point, 'y_cord':float(y_val)})
+
+    temp_df = data_frame[data_frame['target'] == start_point].reset_index()
+    assignable_range = list(range(min_range, max_range+1))
+    for i, row in temp_df.iterrows():
+        needed_slots = row['max_downstream_cluster_size']
+        use_range = assignable_range[:needed_slots]
+        new_min_range = min(use_range)
+        new_max_range = max(use_range)
+        assignable_range = assignable_range[needed_slots:]
+        next_start_point = row['node']
+        assign_y_values(data_frame=data_frame, start_point=next_start_point, min_range=new_min_range, max_range=new_max_range, y_values_list=y_values_list)
+    return y_values_list
+    
+
+def assign_y_from_range(min_range:int, max_range:int) -> float:
+    """Assign the Y cordinate by adding the branch minimum and
+    maximum range values then divide by 2.
+
+    Args:
+        min_range (int): Minimum value of the branch range.
+        max_range (int): Maximum value of the branch range.
+
+    Returns:
+        float: The Y cordinate of the node.
+    """
+    return (max_range+min_range)/2
 
 
 file_path = f'C:/Users/Kyle/downloads/node_edge.csv'
@@ -48,66 +140,9 @@ start_points_df = node_edge_df[node_edge_df['end_point'].isnull()]
 # the nodes position to keep evenly spaced...
 #
 
-
-def test_x_y(data_frame: pd.DataFrame, start_point:str, _x:int = 1, _y:int=1, _data:list = None):
-    temp_df = data_frame[data_frame['end_point'] == start_point].reset_index()
-    node_cluster_list = "_".join(temp_df['start_point'].to_list())
-    #node_cluster_list = temp_df['start_point'].to_list()
-    levels_count = len(temp_df) if len(temp_df) > 0 else 1
-    if levels_count == 1:
-        pass
-    elif levels_count %2 == 0:
-        _y = _y-levels_count/2
-    else:
-        numbers_list = list(range(1, levels_count+1))
-        _y = _y - int(np.median(numbers_list))
-    for i, row in temp_df.iterrows():
-        next_start_point = row['start_point']
-        node_cords = {'node':next_start_point, 'x_cord':_x, 'y_cord':(_y+i), 'target':start_point, 'node_cluster_count': levels_count, 'node_cluster_list': node_cluster_list, 'branch_order': i+1}
-        if _data:
-            _data.append(node_cords)
-        else:
-            _data = [node_cords]
-        test_x_y(data_frame=data_frame, start_point=next_start_point, _x=_x+1, _y=_y+i, _data=_data)
-    return _data
-
-def total_nodes_in_branch(data_frame: pd.DataFrame, start_point:str):
-    temp_df = data_frame[data_frame['end_point'] == start_point].reset_index()
-    total_count = len(temp_df) if len(temp_df) > 0 else 1
-    down_stream_cnt = 0
-    for i, row in temp_df.iterrows():
-        next_start_point = row['start_point']
-        max_down_nodes = total_nodes_in_branch(data_frame=data_frame, start_point=next_start_point)
-        down_stream_cnt += max_down_nodes
-    return total_count if total_count > down_stream_cnt else down_stream_cnt
-    
-
-def assign_y_values(data_frame:pd.DataFrame, start_point:str, min_range:int, max_range: int, y_values_list: list = None):
-    y_val = get_y_from_range(min_range=min_range, max_range=max_range)
-    if not y_values_list:
-        y_values_list = []
-    y_values_list.append({'node':start_point, 'y_cord':float(y_val)})
-
-    temp_df = data_frame[data_frame['target'] == start_point].reset_index()
-    assignable_range = list(range(min_range, max_range+1))
-    for i, row in temp_df.iterrows():
-        needed_slots = row['max_downstream_cluster_size']
-        use_range = assignable_range[:needed_slots]
-        new_min_range = min(use_range)
-        new_max_range = max(use_range)
-        assignable_range = assignable_range[needed_slots:]
-        next_start_point = row['node']
-        assign_y_values(data_frame=data_frame, start_point=next_start_point, min_range=new_min_range, max_range=new_max_range, y_values_list=y_values_list)
-    return y_values_list
-    
-
-def get_y_from_range(min_range:int, max_range:int) -> float:
-    return (max_range+min_range)/2
-
-
 for i, sp_row in start_points_df.iterrows():
     original_start_point = sp_row['start_point']
-    result = test_x_y(data_frame=node_edge_df, start_point=original_start_point)
+    result = build_node_x_cords(data_frame=node_edge_df, start_point=original_start_point)
     result.append({'node':original_start_point, 'x_cord':0, 'y_cord':0, 'target':'None', 'node_cluster_count': 1, 'node_cluster_list': original_start_point, 'branch_order': 1})
     df = pd.DataFrame(result)
     df['original_start_point'] = original_start_point
@@ -142,7 +177,7 @@ for i, sp_row in start_points_df.iterrows():
     down_stream_max_nodes_cnt = []
     for i, row in df.iterrows():
         start_point = row['node']
-        check_max = total_nodes_in_branch(node_edge_df, start_point)
+        check_max = get_max_nodes_in_branch(node_edge_df, start_point)
         down_stream_max_nodes_cnt.append({'node': start_point, 'max_downstream_cluster_size': check_max})
     max_nodes_df = pd.DataFrame(down_stream_max_nodes_cnt)
     df = pd.merge(df, max_nodes_df, how='left', on='node')
